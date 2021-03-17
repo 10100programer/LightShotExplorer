c198595925deb9be6cb063a2f6d0b20bcb1d0de6
10100programer <alexmettler@hotmail.com>
2021-03-17 00:24:04 -0500

    first alpha level program still tons of bugs and not very user friendly

--- LightShotExplorer/Form1.cs
+++ LightShotExplorer/Form1.cs
@@ -16,5 +16,37 @@ namespace LightShotExplorer
         {
             InitializeComponent();
         }
+        static PrintScreenNumber PrintScreenNumber;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.ScriptErrorsSuppressed = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+            }
+        }
+        bool firstclick = true;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (firstclick)
+            {
+                PrintScreenNumber = new PrintScreenNumber(textBox1.Text);
+                textBox1.Enabled = false;
+                firstclick = false;
+            }
+            
+            PrintScreenNumber.NextPage();
+            textBox1.Text = PrintScreenNumber.PageURL;
+            webBrowser1.Url = new Uri(textBox1.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
